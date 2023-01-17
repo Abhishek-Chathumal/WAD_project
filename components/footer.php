9870cb968e085012bb2d7c7cd4d891010268613c
Abhishek Chathumal Wellage <wellage-se19007@stu.kln.ac.lk>
2023-01-17 11:22:08 +0530

    footer and style

--- components/footer.php
+++ components/footer.php
@@ -20,10 +20,10 @@
 
       <div class="box">
          <h3>contact us</h3>
-         <a href="tel:1234567890"><i class="fas fa-phone"></i> +123 456 7899</a>
-         <a href="tel:11122233333"><i class="fas fa-phone"></i> +111 222 3333</a>
-         <a href="mailto:shaikh@gmail.com"><i class="fas fa-envelope"></i> shaikh@gmail.com</a>
-         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> mumbai, india - 400104 </a>
+         <a href=""><i class="fas fa-phone"></i> +94 033 234 6534</a>
+         <a href=""><i class="fas fa-phone"></i> +94 033 234 6535</a>
+         <a href=""><i class="fas fa-envelope"></i> thepartsshop@info.com</a>
+         <a href=""><i class="fas fa-map-marker-alt"></i> Kelaniya - Sri Lanka</a>
       </div>
 
       <div class="box">
@@ -36,6 +36,6 @@
 
    </section>
 
-   <div class="credit">Developed solely for the demonstration purposes by Group 2: SENG 21253 - Web Application Development Final Project @ <?= date('Y'); ?> </div>
+   <div class="credit">Developed solely for the demonstration purposes by Group 2: SENG 21253 - Web Application Development /Final Project @ <?= date('Y'); ?> </div>
 
 </footer>
