3101fef6d1bb437e92c893661d577cce5d3e3b62
Abhishek Chathumal Wellage <wellage-se19007@stu.kln.ac.lk>
2023-01-17 12:22:23 +0530

    modified

--- components/user_header.php
+++ components/user_header.php
@@ -15,7 +15,7 @@
 
    <section class="flex">
 
-      <a href="home.php" class="logo"><img height="80px" width="auto" src="images/logo.png"/> <span>.</span></a>
+      <a href="home.php" class="logo"><img height="80px" width="auto" src="images/logo.png"/> <span></span></a>
 
       <nav class="navbar">
          <a href="home.php">home</a>
